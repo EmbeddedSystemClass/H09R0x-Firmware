45a65d8bde19e065e8d533f7e41f7cd7bb7acd0f
Asaad K <masaadkaadan@gmail.com>
2019-07-21 18:00:23 +0300

    Porting circular buffer updates: - Added BOS_dma to proj - Added HAL CRC to proj - Moving message codes to BOS - Replace DMA, IT and UART mdoule drivers - Update GPIO module driver - Update HAL conf - Increase optimization to O1
    Fix uart/port mapping in module driver

--- H09R0/H09R0_uart.c
+++ H09R0/H09R0_uart.c
@@ -129,7 +129,7 @@ void MX_USART4_UART_Init(void)
   huart4.Init.OneBitSampling = UART_ONEBIT_SAMPLING_DISABLED;
   huart4.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
 	HAL_UART_Init(&huart4);
-	#if _P6pol_reversed	
+	#if _P1pol_reversed	
 		huart4.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_SWAP_INIT;
 		huart4.AdvancedInit.Swap = UART_ADVFEATURE_SWAP_ENABLE;
 	  HAL_UART_Init(&huart4);
@@ -411,6 +411,26 @@ HAL_StatusTypeDef writePxITMutex(uint8_t port, char *buffer, uint16_t n, uint32_
 		/* Wait for the mutex to be available. */
 		if (osSemaphoreWait(PxTxSemaphoreHandle[port], mutexTimeout) == osOK) {
 			result = HAL_UART_Transmit_IT(GetUart(port), (uint8_t *)buffer, n);
+		}
+	}
+	
+	return result;
+}
+
+/* --- Non-blocking (DMA-based) write protected with a semaphore --- 
+*/
+HAL_StatusTypeDef writePxDMAMutex(uint8_t port, char *buffer, uint16_t n, uint32_t mutexTimeout)
+{
+	HAL_StatusTypeDef result = HAL_ERROR; 
+	UART_HandleTypeDef* hUart = GetUart(port);
+
+	if (hUart != NULL) {	
+		/* Wait for the mutex to be available. */
+		if (osSemaphoreWait(PxTxSemaphoreHandle[port], mutexTimeout) == osOK) {
+			/* Setup TX DMA on this port */
+			DMA_MSG_TX_Setup(hUart);
+			/* Transmit the message */
+			result = HAL_UART_Transmit_DMA(hUart, (uint8_t *)buffer, n);
 		}
 	}
 	
